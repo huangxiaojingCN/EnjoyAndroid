79a88c396328ed43e20cb1e895e6c196f890ba6d
huangxiaojing <huangxiaojingcn@gmail.com>
2019-08-29 10:53:38 +0800

    Refactor: 代码结构调整，增加注释.

--- app/src/main/java/com/hxj/enjoyandroid/ui/canvas/HistogramView.java
+++ app/src/main/java/com/hxj/enjoyandroid/ui/canvas/HistogramView.java
@@ -3,7 +3,6 @@ package com.hxj.enjoyandroid.ui.canvas;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.text.TextPaint;
@@ -50,9 +49,37 @@ public class HistogramView<T extends HistogramBean> extends View {
 
     private List<T> datas = new ArrayList<>();
 
-    private Matrix mMatrix;
-
     private TextPaint mTextPaint;
+
+    /**
+     *  x 轴起点x坐标. 这里是为了好理解，实际上 startX == yCoordinateStartX
+     *  因为是原点.
+     */
+    private int startX;
+    /**
+     *  x 轴起点y坐标
+     */
+    private int startY;
+
+    /**
+     *  y 轴终点 x 坐标
+     */
+    private int yCoordinateEndX;
+
+    /**
+     *  y 轴终点 y 坐标
+     */
+    private int yCoordinateEndY;
+
+
+    private int lastX;
+
+    private int avialableSpace;
+
+    private int avaiableColumnSpace;
+
+    private int xScaleStep;
+
 
     public HistogramView(Context context) {
         this(context, null);
@@ -84,21 +111,28 @@ public class HistogramView<T extends HistogramBean> extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+
+        // view 的宽度
         mWidth = w;
+
+        // view 高度.
         mHeight = h;
 
+        // x 轴坐标的宽度
         mXCoordinateWidth = mWidth * 2 / 3;
 
+        // y 轴坐标的高度.
         mYCoordinateHeight = mHeight * 2 /3;
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
+    /**
+     *  绘制 x,y 轴
+     * @param canvas
+     */
+    private void drawXYCoordinate(Canvas canvas) {
         // 绘制 x 轴坐标.
-        int startX = (mWidth - mXCoordinateWidth) / 2;
-        int startY = mHeight - (mHeight - mYCoordinateHeight) / 2;
+        startX = (mWidth - mXCoordinateWidth) / 2;
+        startY = mHeight - (mHeight - mYCoordinateHeight) / 2;
         int endX = startX + mXCoordinateWidth;
         int endY = startY;
 
@@ -108,8 +142,8 @@ public class HistogramView<T extends HistogramBean> extends View {
         int yCoordinateStartX = startX;
         int yCoordinateStartY = startY;
 
-        int yCoordinateEndX = startX;
-        int yCoordinateEndY = (mHeight - mYCoordinateHeight) / 2;
+        yCoordinateEndX = startX;
+        yCoordinateEndY = (mHeight - mYCoordinateHeight) / 2;
         canvas.drawLine(yCoordinateStartX, yCoordinateStartY, yCoordinateEndX, yCoordinateEndY, mCoordinatePaint);
 
         // x 轴箭头
@@ -127,25 +161,91 @@ public class HistogramView<T extends HistogramBean> extends View {
 
         // y 轴箭头
         int layer1 = canvas.saveLayer(0, 0, mWidth, mHeight, null);
-        canvas.translate(yCoordinateEndX, yCoordinateEndY - 30);
-        canvas.drawLine(0, 30, 0, 0, mCoordinatePaint);
+        canvas.translate(yCoordinateEndX, yCoordinateEndY - 60);
+        canvas.drawLine(0, 60, 0, 0, mCoordinatePaint);
         canvas.rotate(60);
         canvas.drawLine(0, 0, 30, 0, mCoordinatePaint);
 
         canvas.rotate(60);
         canvas.drawLine(0, 0, 30, 0, mCoordinatePaint);
         canvas.restoreToCount(layer1);
+    }
 
-        // 绘制直方图数据.
+    private void drawHistogram(Canvas canvas, int height, int max, String color) {
+        // 根据最大值的比列进行缩放.
+        int relalHeight = mYCoordinateHeight * height / max;
+
+        int left = startX + space + lastX;
+        if (lastX > 0) {
+            left = lastX + space;
+        }
+        int top = (yCoordinateEndY + mYCoordinateHeight - relalHeight);
+        int right = (left + avaiableColumnSpace);
+        int bottom = startY - 3;
+
+        if (!TextUtils.isEmpty(color)) {
+            mPaint.setColor(Color.parseColor(color));
+        }
+
+        canvas.drawRect(left, top, right, bottom, mPaint);
+        lastX = right;
+    }
+
+    private void drawXScaleValue(Canvas canvas, String columnName) {
+        // 绘制 x 轴刻度.
+        int xScalex = 0;
+        if (xScaleStep > 0) {
+            xScalex = xScaleStep;
+        } else {
+            xScalex = startX + space + avaiableColumnSpace / 2;
+        }
+        int xScaley = startY;
+
+        int xScaleEndx = xScalex;
+        int xScaleEndy = startY + 10;
+
+        mPaint.setColor(Color.BLACK);
+        canvas.drawLine(xScalex, xScaley, xScaleEndx, xScaleEndy, mPaint);
+
+        xScaleStep = xScalex + 2 * avaiableColumnSpace / 2 + space;
+
+        // 绘制 x 轴刻度值.
+        Rect columnNameRect = new Rect();
+        mTextPaint.getTextBounds(columnName, 0, columnName.length(), columnNameRect);
+        canvas.drawText(columnName,
+                xScalex - columnNameRect.width() / 2,
+                xScaleEndy + columnNameRect.height(),
+                mTextPaint);
+    }
+
+    private void drawYScaleValue(Canvas canvas, int yScaleValue, String yScaleValueStr) {
+        // 绘制 y 轴刻度.
+        // 根据列的数量进行刻度实现.
+        Log.i(TAG, "onDraw yScaleValue: " + yScaleValue);
+        int yScaley = yCoordinateEndY + mYCoordinateHeight - yScaleValue;
+        int yScalex = yCoordinateEndX;
+
+        int yScaleEndx = yCoordinateEndX - 10;
+        int yScaleEndy = yScaley;
+
+        mPaint.setColor(Color.BLACK);
+        canvas.drawLine(yScalex, yScaley, yScaleEndx, yScaleEndy, mPaint);
+
+        // 绘制 y 轴刻度值.
+        Log.i(TAG, "onDraw yScaleValueStr: " + yScaleValueStr);
+        Rect textRect = new Rect();
+        mTextPaint.getTextBounds(yScaleValueStr, 0, yScaleValueStr.length(), textRect);
+        canvas.drawText(yScaleValueStr, startX - textRect.width() - 20, yScaley + textRect.height() / 2, mTextPaint);
+    }
+
+    private void drawColumn(Canvas canvas) {
         // 得到直方图列可绘制区域
-        int avialableSpace = mXCoordinateWidth - totoalSpaces * space;
-        // 列的可用控件
-        int avaiableColumnSpace = avialableSpace / columns;
+        avialableSpace = mXCoordinateWidth - totoalSpaces * space;
+        // 列的可用宽度
+        avaiableColumnSpace = avialableSpace / columns;
 
         // 计算最高数值与坐标轴实际高度的比值，
         if (datas.size() > 0) {
-            int lastX = 0;
-            int xScaleStep = 0;
             for (int i = 0; i < columns; i++) {
                 HistogramBean bean =  datas.get(i);
                 int height = bean.height;
@@ -156,68 +256,19 @@ public class HistogramView<T extends HistogramBean> extends View {
                 if (max == 0 || max < height) {
                     throw new IllegalStateException("最大值不能为0");
                 }
-                // 按着最大值的比列进行缩放.
-                int relalHeight = mYCoordinateHeight * height / max;
 
-                int left = startX + space + lastX;
-                if (lastX > 0) {
-                    left = lastX + space;
-                }
-                int top = (yCoordinateEndY + mYCoordinateHeight - relalHeight);
-                int right = (left + avaiableColumnSpace);
-                int bottom = startY;
-
-                if (!TextUtils.isEmpty(color)) {
-                    mPaint.setColor(Color.parseColor(color));
-                }
-                canvas.drawRect(left, top, right, bottom, mPaint);
-                lastX = right;
-
+                // 绘制直方图.
+                drawHistogram(canvas, height, max, color);
 
                 // 绘制 x 轴刻度.
-                int xScalex = 0;
-                if (xScaleStep > 0) {
-                    xScalex = xScaleStep;
-                } else {
-                    xScalex = startX + space + avaiableColumnSpace / 2;
-                }
-                int xScaley = startY;
+                drawXScaleValue(canvas, columnName);
 
-                int xScaleEndx = xScalex;
-                int xScaleEndy = startY + 10;
-
-                mPaint.setColor(Color.RED);
-                canvas.drawLine(xScalex, xScaley, xScaleEndx, xScaleEndy, mPaint);
-
-                xScaleStep = xScalex + 2 * avaiableColumnSpace / 2 + space;
-
-                // 绘制 x 轴刻度值.
-                Rect columnNameRect = new Rect();
-                mTextPaint.getTextBounds(columnName, 0, columnName.length(), columnNameRect);
-                canvas.drawText(columnName,
-                        xScalex - columnNameRect.width() / 2,
-                        xScaleEndy + columnNameRect.height(),
-                        mTextPaint);
-
-                // 绘制 y 轴刻度.
-                // 根据列的数量进行刻度实现.
+                // 根据列以及最大值来分配刻度值，可以通过计算更换刻度的密度.
                 int yScaleValue = (i + 1) * max / columns * mYCoordinateHeight / max;
-                Log.i(TAG, "onDraw yScaleValue: " + yScaleValue);
-                int yScaley = yCoordinateEndY + mYCoordinateHeight - yScaleValue;
-                int yScalex = yCoordinateEndX;
-
-                int yScaleEndx = yCoordinateEndX - 10;
-                int yScaleEndy = yScaley;
-
-                canvas.drawLine(yScalex, yScaley, yScaleEndx, yScaleEndy, mPaint);
-
-                // 绘制 y 轴刻度值.
                 String yScaleValueStr = (i + 1) * max / columns + "";
-                Log.i(TAG, "onDraw yScaleValueStr: " + yScaleValueStr);
-                Rect textRect = new Rect();
-                mTextPaint.getTextBounds(yScaleValueStr, 0, yScaleValueStr.length(), textRect);
-                canvas.drawText(yScaleValueStr, startX - textRect.width() - 20, yScaley + textRect.height() / 2, mTextPaint);
+                drawYScaleValue(canvas, yScaleValue, yScaleValueStr);
             }
+
             // 补画 y 轴 0 刻度
             int yScale0x = yCoordinateEndX;
             int yScale0y = mYCoordinateHeight + yCoordinateEndY;
@@ -229,9 +280,21 @@ public class HistogramView<T extends HistogramBean> extends View {
             String yScaleValueStr = "0";
             Rect textRect = new Rect();
             mTextPaint.getTextBounds(yScaleValueStr, 0, yScaleValueStr.length(), textRect);
-            canvas.drawText(yScaleValueStr, startX - textRect.width() - 20, yScale0y + textRect.height() / 2, mTextPaint);
+            canvas.drawText(yScaleValueStr,
+                    startX - textRect.width() - 20,
+                    yScale0y + textRect.height() / 2,
+                    mTextPaint);
 
         }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        drawXYCoordinate(canvas);
+
+        drawColumn(canvas);
     }
 
     public void addData(List<T> datas) {
