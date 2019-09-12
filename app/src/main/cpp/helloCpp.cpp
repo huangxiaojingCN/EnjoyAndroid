e2aed4da82787e82074dcc48ed03a22eb278781c
huangxiaojing <huangxiaojingcn@gmail.com>
2019-09-12 10:18:10 +0800

    Feat: cpp 中加入调试.

--- app/src/main/cpp/helloCpp.cpp
+++ app/src/main/cpp/helloCpp.cpp
@@ -4,13 +4,16 @@
 
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 using namespace std;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_hxj_enjoyandroid_fragments_ndk_CMakeListFragment_helloJni(JNIEnv *env, jobject instance) {
 
-    string text = "66666";
+    string text = "牛叉的cpp";
+
+    __android_log_print(ANDROID_LOG_DEBUG, "HelloJni", "text: %s", text.c_str());
 
     return env->NewStringUTF(text.c_str());
 }
